cc5e3a4da3ee0c6623cc0903816282a1d421dc7d
Martin Zlatkin <mzlatkin@elltechnologies.com>
2016-07-21 20:49:34 -0400

    added things

--- pipinventory/fatman/views.py
+++ pipinventory/fatman/views.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.models import User, Group
 from rest_framework import viewsets
-from pipinventory.fatman.serializers import UserSerializer, GroupSerializer
+from fatman.serializers import UserSerializer, GroupSerializer
 
 
 class UserViewSet(viewsets.ModelViewSet):
