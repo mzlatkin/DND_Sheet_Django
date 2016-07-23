5e9291e1fad51a7bb88e2a810596f7b0214f7d32
Martin Zlatkin <mzlatkin@elltechnologies.com>
2016-07-22 22:12:09 -0400

    static stuff

--- pipinventory/pipinventory/settings.py
+++ pipinventory/pipinventory/settings.py
@@ -81,11 +81,14 @@ WSGI_APPLICATION = 'pipinventory.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'pipinventory',
+        'USER': 'pipboy-user',
+        'PASSWORD': 'grasscans',
+        'HOST': 'localhost',
+        'PORT':''
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/1.9/ref/settings/#auth-password-validators
